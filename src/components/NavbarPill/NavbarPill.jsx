718076c5f318edf3c3c4770541ffba6315a317cd
sijincodes <sijinmathews03@gmail.com>
2022-12-12 19:43:08 +0100

    Partial Navbar Component

---
+++ src/components/NavbarPill/NavbarPill.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./NavbarPill.css"
+function NavbarPill() {
+  return (
+    <>
+         <button className="pillHomePage" type="button">
+                pennyWise
+                <br />
+                <span className="span-textHomepage"> TRACK YOUR EXPENSES BETTER</span>
+              </button>
+    </>
+  )
+}
+
+export default NavbarPill
