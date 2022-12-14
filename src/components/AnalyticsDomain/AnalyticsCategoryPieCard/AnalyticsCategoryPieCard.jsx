3ca3a60fab223ea13a18d675060ef4f7d57f9af0
sijincodes <sijinmathews03@gmail.com>
2022-12-15 00:04:36 +0100

    category name display bug fix

--- src/components/AnalyticsDomain/AnalyticsCategoryPieCard/AnalyticsCategoryPieCard.jsx
+++ src/components/AnalyticsDomain/AnalyticsCategoryPieCard/AnalyticsCategoryPieCard.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 import AnalyticsPieGraph from "./../AnalyticsPieGraph/AnalyticsPieGraph";
+import { baseUrl } from "../../../consts";
 
 import "./AnalyticsCategoryPieCard.css";
 
+function fetchCategory(categories, categoryId) {
+  return categories.filter((category) => category._id === categoryId)[0];
+}
 function AnalyticsCategoryPieCard({ pie, category, amount, percent }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategoryDetails = async () => {
+      const authToken = localStorage.getItem("authToken");
+      let categories = await axios.get(`${baseUrl}/category?type=expense`, {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+      setCategories(categories.data);
+    };
+    fetchCategoryDetails();
+  }, [category]);
+
   return (
     <>
       <Container className="analyticsCategoryPieCardContainer">
@@ -13,7 +31,7 @@ function AnalyticsCategoryPieCard({ pie, category, amount, percent }) {
             <AnalyticsPieGraph percent={percent} />
           </Col>
           <Col className="pieCardDetails">
-            <span>{category}</span>
+            <span>{fetchCategory(categories, category)?.categoryName}</span>
             <span>{amount}</span>
           </Col>
           <Col className="pieCardPercentValue">
